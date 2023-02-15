7dff7b39d05a52b7c5dec6f4106190dfb9fca6c7
susie <shuxinli0512@outlook.com>
2023-02-15 15:12:30 -0800

    checking in initial code for stock app

---
+++ NEUStockApp/NEUStockApp/Utills/Constant.swift
@@ -0,0 +1,11 @@
+//
+//  Constant.swift
+//  NEUStockApp
+//
+//  Created by shuxin on 2/11/23.
+//
+
+import Foundation
+
+let apikey = "f468dbda8be0185b0a65634da024b7f7"
+let baseURL = "https://financialmodelingprep.com/api/v3/quote-short/"
